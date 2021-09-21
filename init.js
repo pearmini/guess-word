50eb0a442e84a166378ff87c0d0f5d97ef0f576e
hustcc <i@hust.cc>
2021-09-21 14:47:01 +0800

    feat: for old browser

---
+++ init.js
@@ -0,0 +1,89 @@
+var container = document.querySelector("#word-container > span");
+var words = unique(DICT);
+var currentIndex = -1;
+
+nextWord();
+alert(
+  `点击屏幕右边切换到下一个词，\n点击屏幕左边切换到上一个词，\n目前一共有${words.length}个词语。`
+);
+
+if (platform().isPc) {
+  window.addEventListener("click", onClick);
+} else {
+  window.addEventListener("touchstart", onClick);
+}
+
+window.addEventListener("keydown", function(e) {
+  if (e.key === "Enter") {
+    enterFullScreen();
+  }
+});
+
+function onClick(e) {
+  if (e.pageX < window.innerWidth / 2) {
+    previousWord();
+  } else {
+    nextWord();
+  }
+}
+
+function unique(list) {
+  var arr = [];
+  var map = {};
+  for (let i = 0; i < list.length; i++) {
+    const element = list[i];
+    if (!map[element]) {
+      arr.push(element);
+      map[element] = 1;
+    }
+  }
+  return arr;
+}
+
+function enterFullScreen() {
+  document.documentElement.requestFullscreen();
+}
+
+function updateWord(index) {
+  var word = words[index];
+  container.textContent = word;
+}
+
+function platform() {
+  var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isChrome = /(?:Chrome|CriOS)/.test(ua),
+    isTabvar =
+      /(?:iPad|PlayBook)/.test(ua) ||
+      (isAndroid && !/(?:Mobile)/.test(ua)) ||
+      (isFireFox && /(?:Tabvar)/.test(ua)),
+    isPhone = /(?:iPhone)/.test(ua) && !isTabvar,
+    isPc = !isPhone && !isAndroid && !isSymbian;
+  return {
+    isTabvar: isTabvar,
+    isPhone: isPhone,
+    isAndroid: isAndroid,
+    isPc: isPc,
+  };
+}
+
+function nextWord() {
+  currentIndex++;
+  if (currentIndex >= words.length) {
+    alert("没有更多的词了～");
+    return;
+  }
+  updateWord(currentIndex);
+}
+
+function previousWord() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    alert("已经第一个词了～");
+    return;
+  }
+  updateWord(currentIndex);
+}
